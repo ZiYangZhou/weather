abf6bf7370ecdaac6af9c83281d93d9367a17769
zhouxu <yibinzx@gmail.com>
2016-03-22 22:09:57 +0800

    处理http请求的类，使用和风天气api获取天气。

--- app/src/main/java/zhouxu/weather/http/HttpRequest.java
+++ app/src/main/java/zhouxu/weather/http/HttpRequest.java
@@ -1,9 +1,12 @@
 package zhouxu.weather.http;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
-import zhouxu.weather.util.NetwirkUtil;
-
+import zhouxu.weather.util.NetworkUtil;
 /**
  * @author：Nazgol. Created at 2016/3/21.
  */
@@ -18,18 +21,36 @@ public class HttpRequest {
         return instance;
     }
 
+    /**
+     * 如果未得到返回数据则返回空
+     * @param cityID
+     * @return
+     */
     public String getJsonByCityId(String cityID) {
-        String jsonResult = null;
-        String httpUrl = NetwirkUtil.httpUrlHead + cityID + NetwirkUtil.key;
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+        String httpUrl = NetworkUtil.httpUrlHead + cityID + NetworkUtil.key;
         /**
          * to do
          */
         try {
             URL url = new URL(httpUrl);
-
-        }catch (Exception e) {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
 
         }
-        return jsonResult;
+        return result;
     }
 }
--- app/src/main/java/zhouxu/weather/util/JsonUtil.java
+++ app/src/main/java/zhouxu/weather/util/JsonUtil.java
@@ -1,7 +1,27 @@
 package zhouxu.weather.util;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 /**
  * @author：Nazgol. Created at 2016/3/21.
  */
 public class JsonUtil {
+
+    /**
+     *
+     * @param paramJsonStr
+     * @return
+     * @throws JSONException
+     * @throws Exception
+     */
+    public static JSONObject getJSON(String paramJsonStr) {
+        try {
+            return new JSONObject(paramJsonStr);
+        } catch (JSONException e) {
+
+        } finally {
+            return null;
+        }
+
+    }
+
 }
